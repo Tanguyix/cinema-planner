52aad490a0b6526fca991689972d910e15bea61a
Tanguyix <tboisseldallier@gmail.com>
2022-02-19 16:27:34 +0100

    [FEAT] super basic display of movies shown today in Les Halles

--- src/ugc_planner.py
+++ src/ugc_planner.py
@@ -1,11 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
-driver.get("https://www.ugc.fr/cinema.html?id=10")
 
-def parseMoviesInOneCinema():
-  print("test")
+def displayMovieInfos(movieBlock):
+  try:
+    movieTitleBlock = movieBlock.find_element(By.CSS_SELECTOR, ".block--title")
+    movieTitle = movieTitleBlock.find_element(By.CSS_SELECTOR, "a")
+  except NoSuchElementException:
+    pass
+  else:
+    print(movieTitle.get_attribute("innerHTML"))
+
+def parseMoviesInLesHallesToday():
+  driver.get("https://www.ugc.fr/cinema.html?id=10")
+  try:
+    movieBlocks = driver.find_elements(By.CSS_SELECTOR, ".slider-item .row")
+    for movieBlock in movieBlocks:
+      displayMovieInfos(movieBlock);
+  except NoSuchElementException:
+    pass
 
 
-parseMoviesInOneCinema();
+parseMoviesInLesHallesToday();
