06bc6cbe71a58affd161d304d6a258a49871a2d4
Tanguyix <tboisseldallier@gmail.com>
2022-02-21 21:31:36 +0100

    [FEAT] create all possible plans

--- src/day_planner.py
+++ src/day_planner.py
@@ -1,5 +1,9 @@
 from time_helper import *
 
+maxMustWatch = 0
+maxShouldWatch = 0
+maxBreakTime = 0
+
 def removeUnavailableHoursForOneType(movies, availableHours):
   for movie in movies:
     movie["movieTimes"] = [movieTime for movieTime in movie["movieTimes"] if isAfter(movieTime["startTime"], availableHours["startHour"]) and isBefore(movieTime["endTime"], availableHours["endHour"])]
@@ -10,19 +14,47 @@ def removeUnavailableHours(movies, availableHours):
     "startHour": removeMinutes(int(availableHours["startHour"][0:2]), int(availableHours["startHour"][3:5]), 10),
     "endHour": addMinutes(int(availableHours["endHour"][0:2]), int(availableHours["endHour"][3:5]), 10)
   }
-  cleanMustWatch = removeUnavailableHoursForOneType(movies['mustWatchAnswer'], availableHoursWithSomeMargin)
-  cleanMayWatch = removeUnavailableHoursForOneType(movies['mayWatchAnswer'], availableHoursWithSomeMargin)
+  cleanMustWatch = removeUnavailableHoursForOneType(movies["mustWatchAnswer"], availableHoursWithSomeMargin)
+  cleanMayWatch = removeUnavailableHoursForOneType(movies["mayWatchAnswer"], availableHoursWithSomeMargin)
   return {
-    "mustWatchAnswers": cleanMustWatch,
-    "mayWatchAnswers": cleanMayWatch
+    "mustWatchAnswer": cleanMustWatch,
+    "mayWatchAnswer": cleanMayWatch
   }
 
-def planDay(movies, availableHours):
+def groupAllMovies(movies):
+  for mustWatchMovie in movies["mustWatchAnswer"]:
+    mustWatchMovie["mustWatch"] = True
+  for mayWatchMovie in movies["mayWatchAnswer"]:
+    mayWatchMovie["mustWatch"] = False
+  return movies["mustWatchAnswer"] + movies["mayWatchAnswer"]
+
+
+def duplicateList(list, nbDuplicates):
+  ret = []
+  for _ in range(0, nbDuplicates):
+    ret.append(list.copy())
+  return ret
+
+def addMovieToPlan(plan, movie):
+  newPlan = []
+  for i in range(len(plan)):
+    newPlan = newPlan + duplicateList(plan[i], len(movie["movieTimes"]))
+  for j in range(len(newPlan)):
+    newPlan[j].append({"title": movie["title"], "mustWatch": movie["mustWatch"], "movieTime": movie["movieTimes"][j % len(movie["movieTimes"])] })
+  return newPlan
+
+def createAllPlans(movies):
   plan = []
-  maxMustWatch = 0
-  maxShouldWatch = 0
-  maxBreakTime = 0
-  moviesInAvailableHours = removeUnavailableHours(movies, availableHours)
-  # Remove unavailable hours for all movies (ie run through all movie times in both must and may_watch, if (start before available StartHour or end after available endHour, remove it, with a few minutes margin))
-  # Loop through all movies, then all available hours, test every case, pass if collision, if mustWatch > previousMax, ou == et mayWatch > max ou == et breakTime > maxBreak, nouveau plan, sinon do nothing
+  for i in range(len(movies[0]["movieTimes"])):
+    plan.append([{ "title": movies[0]["title"], "mustWatch": movies[0]["mustWatch"], "movieTime": movies[0]["movieTimes"][i]}])
+  for j in range(1, len(movies)):
+    plan = addMovieToPlan(plan, movies[j])
+  print(len(plan))
   return plan
+
+def planDay(movies, availableHours):
+  moviesInAvailableHours = removeUnavailableHours(movies, availableHours)
+  allMovies = groupAllMovies(moviesInAvailableHours)
+  graph = createAllPlans(allMovies)
+  print(graph)
+  # return plan
