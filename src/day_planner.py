61742a82c74b2532fa7257e83ce81904efd3d9bd
Tanguyix <tboisseldallier@gmail.com>
2022-02-21 14:46:07 +0100

    [FEAT] remove movie times outside of available hours

--- src/day_planner.py
+++ src/day_planner.py
@@ -1,19 +1,28 @@
-from time_helper import removeMinutes, addMinutes
+from time_helper import *
 
-def removeUnuvailableHours(movies, availableHours):
+def removeUnavailableHoursForOneType(movies, availableHours):
+  for movie in movies:
+    movie["movieTimes"] = [movieTime for movieTime in movie["movieTimes"] if isAfter(movieTime["startTime"], availableHours["startHour"]) and isBefore(movieTime["endTime"], availableHours["endHour"])]
+  return movies
+
+def removeUnavailableHours(movies, availableHours):
   availableHoursWithSomeMargin = {
     "startHour": removeMinutes(int(availableHours["startHour"][0:2]), int(availableHours["startHour"][3:5]), 10),
     "endHour": addMinutes(int(availableHours["endHour"][0:2]), int(availableHours["endHour"][3:5]), 10)
   }
-  print(availableHoursWithSomeMargin)
+  cleanMustWatch = removeUnavailableHoursForOneType(movies['mustWatchAnswer'], availableHoursWithSomeMargin)
+  cleanMayWatch = removeUnavailableHoursForOneType(movies['mayWatchAnswer'], availableHoursWithSomeMargin)
+  return {
+    "mustWatchAnswers": cleanMustWatch,
+    "mayWatchAnswers": cleanMayWatch
+  }
 
 def planDay(movies, availableHours):
   plan = []
   maxMustWatch = 0
   maxShouldWatch = 0
   maxBreakTime = 0
-  moviesInAvailableHours = removeUnuvailableHours(movies, availableHours)
-  print("available Hours = ", availableHours, "movies = ", movies)
+  moviesInAvailableHours = removeUnavailableHours(movies, availableHours)
   # Remove unavailable hours for all movies (ie run through all movie times in both must and may_watch, if (start before available StartHour or end after available endHour, remove it, with a few minutes margin))
   # Loop through all movies, then all available hours, test every case, pass if collision, if mustWatch > previousMax, ou == et mayWatch > max ou == et breakTime > maxBreak, nouveau plan, sinon do nothing
   return plan
--- src/time_helper.py
+++ src/time_helper.py
@@ -2,16 +2,27 @@ def removeMinutes(hours, minutes, minutesToRemove):
   if (minutesToRemove > 60):
     raise Exception("should not try to remove more than 60 minutes") 
   if (minutesToRemove < minutes):
-    return str(hours) + ":" + str(minutes - minutesToRemove)
+    return ("0" + str(hours) + ":" + str(minutes - minutesToRemove))[-5:]
   if (hours - 1 > 0):
-    return str(hours - 1) + ":" + str(60 + minutes - minutesToRemove)
+    return ("0" + str(hours - 1) + ":" + str(60 + minutes - minutesToRemove))[-5:]
   return "23:" + str(60 + minutes - minutesToRemove)
 
 def addMinutes(hours, minutes, minutesToAdd):
   if (minutesToAdd > 60):
     raise Exception("should not try to add more than 60 minutes") 
   if (minutesToAdd + minutes < 60):
-    return str(hours) + ":" + str(minutes + minutesToAdd)
+    return ("0" + str(hours) + ":" + str(minutes + minutesToAdd))[-5:]
   if (hours + 1 < 24):
-    return str(hours + 1) + ":" + str(minutes + minutesToAdd - 60)
+    return ("0" + str(hours + 1) + ":" + str(minutes + minutesToAdd - 60))[-5:]
   return "00:" + str(minutes + minutesToAdd - 60)
+
+def isAfter(firstTime, secondTime):
+  firstHour = int(firstTime[0:2])
+  firstMinutes = int(firstTime[3:5])
+  secondHour = int(secondTime[0:2])
+  secondMinutes = int(secondTime[3:5])
+  print("ft = ", firstTime, "st = ", secondTime, "isAfter = ", firstHour > secondHour or (firstHour == secondHour and firstMinutes > secondMinutes))
+  return (firstHour > secondHour or (firstHour == secondHour and firstMinutes > secondMinutes))
+
+def isBefore(firstTime, secondTime):
+  return isAfter(secondTime, firstTime)
