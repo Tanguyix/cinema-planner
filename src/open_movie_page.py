d76ae7889f98ba21b1cb6efdcd794c1e3a54e060
Tanguyix <tboisseldallier@gmail.com>
2022-12-27 19:51:35 +0100

    [FEAT] reset cinema list script

--- src/open_movie_page.py
+++ src/open_movie_page.py
@@ -1,15 +1,9 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-# from skip_modals import skipModals
+from skip_modals import skipModals
 
 def openMoviePage(driver, url):
   driver.get(url)
   time.sleep(1) # TODO: better handling of wait
-  #TODO this PR : replace again with skipModals
-  try:
-    driver.find_element(By.CSS_SELECTOR, "#didomi-notice-agree-button").click()
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, "#modal-nl-advertising-rgpd-close").click()
-  except NoSuchElementException:
-    pass
+  skipModals()
---
+++ src/reset_cinema_list.py
@@ -0,0 +1,3 @@
+from get_cinema_list import getCinemaList
+
+getCinemaList()
